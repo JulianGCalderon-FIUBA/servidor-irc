3d133c706a55bfd71fdcfcf9735f55fb6fd53881
solmoon16 <mmoon@fi.uba.ar>
2022-10-02 15:25:58 -0300

    Agrego documentacion sobre los modulos

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+//! # Internet Relay Chat
+//! This crate implements the Internet Relay Chat protocol, specifically RFC1459.
+
+/// This module contains a client's functionality. A client is created connected to a stream; through it the client can send and receive messages.
 pub mod client;
 
+/// This module contains a server's functionality. A server listens through an address waiting to hear from and then handle a client.
 pub mod server;
 
+/// This module contains a message's functionality. A message can be sent to and read from a stream.
 pub mod message;
 
 pub const ADDRESS: &str = "127.0.0.1:9001";
