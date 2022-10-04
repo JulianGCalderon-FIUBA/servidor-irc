2cb8888ee9d799a21565fe8c5d090a1efba7c520
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2022-10-04 00:52:38 -0300

    esqueleto de parsing

--- src/message.rs
+++ src/message.rs
@@ -2,18 +2,31 @@ use std::io::{self, BufRead, BufReader};
 use std::io::{Read, Write};
 
 pub struct Message {
-    content: String,
+    prefix: Option<String>,
+    command: String,
+    parameters: Vec<String>,
 }
+
+type MessageParse = (Option<String>, String, Vec<String>);
 
 const CRLF: &[u8] = b"\r\n";
 
 impl Message {
     pub fn new(content: String) -> Self {
-        Self { content }
+        let parsing = parse(content);
+
+        let (prefix, command, parameters) = parsing;
+
+        Self {
+            prefix,
+            command,
+            parameters,
+        }
     }
 
     pub fn send_to(&self, stream: &mut dyn Write) -> io::Result<()> {
-        let bytes = self.content.as_bytes();
+        let content = self.to_string();
+        let bytes = content.as_bytes();
 
         stream.write_all(bytes)?;
         stream.write_all(CRLF)?;
@@ -31,21 +44,40 @@ impl Message {
             return Ok(None);
         }
 
-        trim_crlf(&mut content);
+        if content.as_bytes().ends_with(CRLF) {
+            content.pop();
+            content.pop();
+        } else {
+            return Ok(None);
+        }
 
-        Ok(Some(Self { content }))
+        Ok(Some(Self::new(content)))
     }
 }
 
 impl std::fmt::Display for Message {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.content)
+        if let Some(prefix) = &self.prefix {
+            write!(f, ":{} ", prefix)?;
+        }
+
+        write!(f, "{}", self.command)?;
+
+        let mut parameters = self.parameters.iter();
+        let last = parameters.next();
+        let last = match last {
+            None => "",
+            Some(parameter) => parameter,
+        };
+
+        for parameter in parameters {
+            write!(f, " {}", parameter)?;
+        }
+
+        write!(f, " :{}", last)
     }
 }
 
-fn trim_crlf(string: &mut String) {
-    if string.as_bytes().ends_with(CRLF) {
-        string.pop();
-        string.pop();
-    }
+fn parse(content: String) -> MessageParse {
+    (None, content, Vec::new())
 }
