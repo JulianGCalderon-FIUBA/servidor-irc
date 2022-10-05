351b93aa54efe9d19b4191aa40ba2967f353c5ee
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2022-10-04 22:40:54 -0300

    FINISH PARSING

--- src/server.rs
+++ src/server.rs
@@ -19,9 +19,7 @@ impl Server {
 
     pub fn listen(&mut self) -> io::Result<()> {
         for client in self.listener.incoming() {
-            if let Err(err) = self.handle_client(client?) {
-                eprintln!("Conection ended: {err}");
-            }
+            self.handle_client(client?)?;
         }
 
         Ok(())
@@ -31,23 +29,12 @@ impl Server {
         loop {
             let message = match Message::read_from(&mut client) {
                 Ok(message) => message,
-                Err(error) => match error {
-                    CreationError::IoError(error) => return Err(error),
-                    CreationError::ParsingError(error) => {
-                        send_response_for_parsing_error(error, &mut client)?;
-                        continue;
-                    }
-                },
+                Err(CreationError::IoError(error)) => return Err(error),
+                Err(CreationError::ParsingError(_)) => continue,
             };
 
             println!("Received: {}", message);
             message.send_to(&mut client)?;
         }
     }
-}
-
-fn send_response_for_parsing_error(error: ParsingError, client: &mut TcpStream) -> io::Result<()> {
-    let error_string = format!("PARSINGERROR :{}", error);
-    let error_message = Message::new(&error_string).expect("Is always valid");
-    error_message.send_to(client)
 }
