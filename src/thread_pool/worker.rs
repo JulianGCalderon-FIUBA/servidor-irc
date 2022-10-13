9162908e2ae58427dfad09d00d079ea4ac4874e5
solmoon16 <mmoon@fi.uba.ar>
2022-10-13 18:44:55 -0300

    cambio en worker

--- src/thread_pool/worker.rs
+++ src/thread_pool/worker.rs
@@ -15,14 +15,16 @@ impl Worker {
     /// Creates new Worker that executes Job received through receiver.
     pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
         let thread = thread::spawn(move || loop {
-            // si usamos otra estructura (while let, if let, match el job no se dropea hasta que termina, haciendo que no pueda haber muchos threads a la vez.)
-            let job = receiver
-                .lock()
-                .expect("Lock is poisoned")
-                .recv()
-                .expect("Sender (threadPool) has disconnected");
-            println!("Worker {id} got a job; executing");
-            job();
+            let message = receiver.lock().expect("Lock is poisoned").recv();
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing");
+                    job();
+                }
+                Err(_) => {
+                    break;
+                }
+            }
         });
 
         Self {
