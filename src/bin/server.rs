7a69017b6b5bb97d69e51912f931c7a742270985
solmoon16 <mmoon@fi.uba.ar>
2022-10-12 23:24:26 -0300

    agregamos client_handler

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -2,7 +2,7 @@ use internet_relay_chat::server::Server;
 use internet_relay_chat::ADDRESS;
 
 fn main() {
-    let mut server = match Server::new(ADDRESS.to_string()) {
+    let server = match Server::new(ADDRESS.to_string()) {
         Ok(server) => server,
         Err(error) => return eprintln!("Error: Binding to address: {:?}", error),
     };
