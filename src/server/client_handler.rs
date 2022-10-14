442ef00255e0a77e53c6d60922c10fa04f7c5836
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2022-10-14 16:36:55 -0300

    simple hub for command executing

--- src/server/client_handler.rs
+++ src/server/client_handler.rs
@@ -1,7 +1,8 @@
-use crate::message::{CreationError, Message};
+use crate::message::{CreationError, Message, ParsingError};
 use crate::server::client_info::ClientInfo;
 use crate::server::Server;
-use std::{io, net::TcpStream};
+use std::io;
+use std::net::TcpStream;
 
 pub struct ClientHandler<'a> {
     /// en el futuro puede ser:
@@ -36,11 +37,28 @@ impl<'a> ClientHandler<'a> {
             let message = match Message::read_from(&mut self.client.stream) {
                 Ok(message) => message,
                 Err(CreationError::IoError(error)) => return Err(error),
-                Err(CreationError::ParsingError(_)) => continue,
+                Err(CreationError::ParsingError(error)) => {
+                    self.on_parsing_error(&error);
+                    continue;
+                }
             };
 
-            println!("Received: {}", message);
+            // let (prefix, command, parameters, trailing) = message.unpack();
+            // match &command[..] {
+            //     "PASS" => self.pass_command(prefix, parameters, trailing),
+            //     "NICK" => self.nick_command(prefix, parameters, trailing),
+            //     "USER" => self.user_command(prefix, parameters, trailing),
+            //     "OPER" => self.oper_command(prefix, parameters, trailing),
+            //     "QUIT" => self.quit_command(prefix, parameters, trailing),
+            //     _ => self.on_unknown_command(&command),
+            // }
+
+            println!("Received: {message}");
             message.send_to(&mut self.client.stream)?;
         }
     }
+
+    fn on_parsing_error(&self, _error: &ParsingError) {}
+
+    // fn on_unknown_command(&self, _command: &str) {}
 }
