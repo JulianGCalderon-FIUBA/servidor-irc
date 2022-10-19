8698e831f960dce8849842218d7f087fdf1399df
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2022-10-18 22:29:09 -0300

    finish

--- src/server/client_handler/connection_info/mod.rs
+++ src/server/client_handler/connection_info/mod.rs
@@ -1,7 +1,8 @@
-use std::net::TcpStream;
+use std::{io, net::TcpStream};
 
 mod registration_state;
 
+use crate::server::database::{ClientInfo, ClientInfoBuilder};
 pub use registration_state::RegistrationState;
 
 /// Holds a Clients' relevant information.
@@ -31,5 +32,23 @@ impl ConnectionInfo {
     }
     pub fn advance_state(&mut self) {
         self.registration_state = self.registration_state.next();
+    }
+
+    pub fn get_client_info(&self) -> io::Result<ClientInfo> {
+        let mut client_builder = ClientInfoBuilder::new_with(
+            self.nickname.clone().unwrap(),
+            self.username.clone().unwrap(),
+            self.hostname.clone().unwrap(),
+            self.servername.clone().unwrap(),
+            self.realname.clone().unwrap(),
+        );
+
+        client_builder.with_stream(self.stream.try_clone()?);
+
+        if let Some(password) = self.password.clone() {
+            client_builder.with_password(password);
+        }
+
+        Ok(client_builder.build())
     }
 }
