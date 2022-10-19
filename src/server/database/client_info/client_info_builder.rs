6893dff2cb4c48ea15fc2d8201806043d488d975
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2022-10-18 22:15:14 -0300

    finish functionalty

---
+++ src/server/database/client_info/client_info_builder.rs
@@ -0,0 +1,51 @@
+use super::ClientInfo;
+use std::net::TcpStream;
+
+pub struct ClientInfoBuilder {
+    stream: Option<TcpStream>,
+    password: Option<String>,
+    nickname: String,
+    username: String,
+    hostname: String,
+    servername: String,
+    realname: String,
+}
+
+impl ClientInfoBuilder {
+    pub fn new_with(
+        nickname: String,
+        username: String,
+        hostname: String,
+        servername: String,
+        realname: String,
+    ) -> Self {
+        Self {
+            stream: None,
+            password: None,
+            nickname,
+            username,
+            hostname,
+            servername,
+            realname,
+        }
+    }
+    pub fn with_stream(&mut self, stream: TcpStream) {
+        self.stream = Some(stream);
+    }
+
+    pub fn with_password(&mut self, password: String) {
+        self.password = Some(password);
+    }
+
+    pub fn build(self) -> ClientInfo {
+        ClientInfo {
+            stream: self.stream,
+            password: self.password,
+            nickname: self.nickname,
+            username: self.username,
+            hostname: self.hostname,
+            servername: self.servername,
+            realname: self.realname,
+        }
+    }
+}
