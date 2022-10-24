e60e767c50cf99b82a2224c0ad9bfeae948068a3
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2022-10-23 21:41:54 -0300

    refactor

--- src/server/client_handler/commands/utils.rs
+++ src/server/client_handler/commands/utils.rs
@@ -5,10 +5,3 @@ fn _is_numeric(a_value: &str) -> bool {
 fn _is_positive_numeric(a_value: &str) -> bool {
     _is_numeric(a_value) && a_value.parse::<isize>().unwrap() >= 0
 }
-
-pub fn pop_times(mut vector: Vec<String>, times: isize) -> String {
-    for _ in 1..times {
-        vector.pop();
-    }
-    vector.pop().unwrap()
-}
