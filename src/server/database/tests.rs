1a23a0e2beaa65f7dd5c1934ad61f639be057018
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2022-10-24 10:48:54 -0300

    refactor

--- src/server/database/tests.rs
+++ src/server/database/tests.rs
@@ -2,7 +2,7 @@ use std::{fs::File, os::unix::prelude::OwnedFd};
 
 use super::*;
 
-fn client_w_nick(nickname: &str) -> ClientInfo {
+fn dummy_client(nickname: &str) -> ClientInfo {
     ClientInfoBuilder::new_with(
         nickname.to_string(),
         "username".to_string(),
@@ -13,13 +13,13 @@ fn client_w_nick(nickname: &str) -> ClientInfo {
     .build()
 }
 
-fn add_stream_to_clientinfo(client_info: &mut ClientInfo, stream: TcpStream) {
+fn add_stream_to_client(client_info: &mut ClientInfo, stream: TcpStream) {
     let stream = Arc::new(Mutex::new(stream));
     client_info.stream = Some(stream);
 }
 
-fn tcp_stream_from_file(path: &str) -> TcpStream {
-    let file = File::open(path).unwrap();
+fn dummy_stream() -> TcpStream {
+    let file = File::open("LICENSE").unwrap();
     let owned_fd = OwnedFd::from(file);
     TcpStream::from(owned_fd)
 }
@@ -29,11 +29,11 @@ fn after_adding_client_database_contains_client() {
     let database = Database::new();
 
     assert!(!database.contains_client("nickname"));
-    database.add_client(client_w_nick("nickname"));
+    database.add_client(dummy_client("nickname"));
     assert!(database.contains_client("nickname"));
 
     assert!(!database.contains_client("nickname2"));
-    database.add_client(client_w_nick("nickname2"));
+    database.add_client(dummy_client("nickname2"));
     assert!(database.contains_client("nickname2"));
 }
 
@@ -41,7 +41,7 @@ fn after_adding_client_database_contains_client() {
 fn after_setting_server_operator_client_is_server_operator() {
     let database = Database::new();
 
-    database.add_client(client_w_nick("nickname"));
+    database.add_client(dummy_client("nickname"));
 
     assert!(!database._is_server_operator("nickname"));
     database.set_server_operator("nickname");
@@ -52,9 +52,9 @@ fn after_setting_server_operator_client_is_server_operator() {
 fn get_stream_returns_reference_to_client_stream() {
     let database = Database::new();
 
-    let mut client = client_w_nick("nickname");
-    let stream = tcp_stream_from_file("assets/dummy_stream");
-    add_stream_to_clientinfo(&mut client, stream);
+    let mut client = dummy_client("nickname");
+    let stream = dummy_stream();
+    add_stream_to_client(&mut client, stream);
 
     let stream_ref_expected = client.get_stream().unwrap();
 
@@ -65,13 +65,23 @@ fn get_stream_returns_reference_to_client_stream() {
 }
 
 #[test]
+fn when_offline_get_stream_returns_none() {
+    let database = Database::new();
+
+    let client = dummy_client("nickname");
+    database.add_client(client);
+
+    assert!(database.get_stream("nickname").is_none());
+}
+
+#[test]
 fn disconnect_client_sets_stream_to_none() {
     let database = Database::new();
 
-    let mut client = client_w_nick("nickname");
-    let stream = tcp_stream_from_file("assets/dummy_stream");
+    let mut client = dummy_client("nickname");
+    let stream = dummy_stream();
 
-    add_stream_to_clientinfo(&mut client, stream);
+    add_stream_to_client(&mut client, stream);
 
     database.add_client(client);
 
@@ -84,7 +94,7 @@ fn disconnect_client_sets_stream_to_none() {
 fn after_adding_client_to_channel_it_exists() {
     let database = Database::new();
 
-    let client = client_w_nick("nickname1");
+    let client = dummy_client("nickname1");
     database.add_client(client);
 
     assert!(!database.contains_channel("channel"));
@@ -93,13 +103,24 @@ fn after_adding_client_to_channel_it_exists() {
 }
 
 #[test]
+fn after_adding_client_to_channel_it_contains_client() {
+    let database = Database::new();
+
+    let client = dummy_client("nickname");
+    database.add_client(client);
+    database.add_client_to_channel("nickname", "channel");
+
+    assert!(database.is_client_in_channel("nickname", "channel"));
+}
+
+#[test]
 fn get_clients_returns_all_clients_from_channel() {
     let database = Database::new();
 
-    let client = client_w_nick("nickname1");
+    let client = dummy_client("nickname1");
     database.add_client(client);
 
-    let client = client_w_nick("nickname2");
+    let client = dummy_client("nickname2");
     database.add_client(client);
 
     database.add_client_to_channel("nickname1", "channel");
@@ -116,10 +137,10 @@ fn get_clients_returns_all_clients_from_channel() {
 fn after_removing_client_from_channel_it_no_longer_contains_client() {
     let database = Database::new();
 
-    let client = client_w_nick("nickname1");
+    let client = dummy_client("nickname1");
     database.add_client(client);
 
-    let client = client_w_nick("nickname2");
+    let client = dummy_client("nickname2");
     database.add_client(client);
 
     database.add_client_to_channel("nickname1", "channel");
@@ -136,7 +157,7 @@ fn after_removing_client_from_channel_it_no_longer_contains_client() {
 fn after_removing_last_client_from_channel_it_no_longer_exists() {
     let database = Database::new();
 
-    let client = client_w_nick("nickname1");
+    let client = dummy_client("nickname1");
     database.add_client(client);
 
     database.add_client_to_channel("nickname1", "channel");
@@ -146,21 +167,10 @@ fn after_removing_last_client_from_channel_it_no_longer_exists() {
 }
 
 #[test]
-fn after_adding_client_to_channel_it_contains_client() {
-    let database = Database::new();
-
-    let client = client_w_nick("nickname");
-    database.add_client(client);
-    database.add_client_to_channel("nickname", "channel");
-
-    assert!(database.is_client_in_channel("nickname", "channel"));
-}
-
-#[test]
 fn get_channels_returns_all_channels() {
     let database = Database::new();
 
-    let client = client_w_nick("nickname");
+    let client = dummy_client("nickname");
     database.add_client(client);
     database.add_client_to_channel("nickname", "channel1");
     database.add_client_to_channel("nickname", "channel2");
@@ -177,7 +187,7 @@ fn get_channels_returns_all_channels() {
 fn get_channels_for_client_returns_all_channels_for_client() {
     let database = Database::new();
 
-    let client = client_w_nick("nickname");
+    let client = dummy_client("nickname");
     database.add_client(client);
     database.add_client_to_channel("nickname", "channel1");
     database.add_client_to_channel("nickname", "channel2");
