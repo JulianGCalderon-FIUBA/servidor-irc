883ef14ef3bd36a7cef0debe77a9af8f5ae80685
solmoon16 <mmoon@fi.uba.ar>
2022-10-26 00:51:15 -0300

    refactor

--- src/server/client_handler/responses/replies.rs
+++ src/server/client_handler/responses/replies.rs
@@ -4,18 +4,33 @@ use crate::server::client_handler::{
 use std::io;
 
 impl ClientHandler {
+    pub fn nickname_in_use_reply(&mut self) -> io::Result<()> {
+        let response = "433 :nickname is already in use".to_string();
+        self.send_response(&response)
+    }
+
+    pub fn nickname_collision_reply(&mut self) -> io::Result<()> {
+        let response = "436 :nickname collision KILL".to_string();
+        self.send_response(&response)
+    }
+
+    pub fn already_registered_reply(&mut self) -> io::Result<()> {
+        let response = "462 :may not reregister".to_string();
+        self.send_response(&response)
+    }
+
     pub fn quit_reply(&mut self, message: &str) -> io::Result<()> {
         let response = format!("{QUIT_COMMAND} :{message}");
         self.send_response(&response)
     }
 
     pub fn ok_reply(&mut self) -> io::Result<()> {
-        let response = "300 :success".to_string();
+        let response = "200 :success".to_string();
         self.send_response(&response)
     }
 
     pub fn list_reply(&mut self, channels: Vec<String>) -> io::Result<()> {
-        let response = "300 :".to_string() + &channels.join(", ");
+        let response = format!("322 : {}", channels.join(", "));
         self.send_response(&response)
     }
 
@@ -25,7 +40,7 @@ impl ClientHandler {
     // }
 
     pub fn no_topic_reply(&mut self, channel: &str) -> io::Result<()> {
-        let response = format!("331 {} :no topic is set", channel);
+        let response = format!("331 {channel} :no topic is set");
         self.send_response(&response)
     }
 
@@ -35,12 +50,12 @@ impl ClientHandler {
     // }
 
     pub fn invite_reply(&mut self, channel: &str, nickname: &str) -> io::Result<()> {
-        let response = format!("341 {} {}", channel, nickname);
+        let response = format!("341 {channel} {nickname}");
         self.send_response(&response)
     }
 
     pub fn names_reply(&mut self, channel: String, clients: Vec<String>) -> io::Result<()> {
-        let response = "353 :".to_string() + &channel + " :" + &clients.join(", ");
+        let response = format!("353 {} :{}", channel, clients.join(" "));
         self.send_response(&response)
     }
 
