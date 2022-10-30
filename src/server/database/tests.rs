f039be1b89d0c487bf92975c068da3b9650f47fa
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2022-10-30 15:24:17 -0300

    testing for getting all clients

--- src/server/database/tests.rs
+++ src/server/database/tests.rs
@@ -1,4 +1,4 @@
-use crate::server::testing_utils::dummy_client;
+use crate::server::testing_utils::{dummy_client, mock_stream::MockTcpStream};
 
 use super::*;
 
@@ -164,16 +164,57 @@ fn get_channels_for_client_returns_all_channels_for_client() {
 }
 
 #[test]
+fn get_clients_for_query_returns_all_matching_clients() {
+    let database = Database::new();
+
+    database.add_client(
+        ClientBuilder::new()
+            .nickname("nickAname".to_string())
+            .username("userBname".to_string())
+            .hostname("hostCname".to_string())
+            .servername("serverDname".to_string())
+            .realname("realEname".to_string())
+            .stream(MockTcpStream::new())
+            .build()
+            .unwrap(),
+    );
+
+    database.add_client(dummy_client("othernick"));
+
+    let expected = vec!["nickAname".to_string()];
+
+    assert_eq!(database.get_clients_for_query("*A*"), expected);
+    assert_eq!(database.get_clients_for_query("*B*"), expected);
+    assert_eq!(database.get_clients_for_query("*C*"), expected);
+    assert_eq!(database.get_clients_for_query("*D*"), expected);
+    assert_eq!(database.get_clients_for_query("*E*"), expected);
+}
+
+#[test]
+fn get_all_clients_returns_all_clients() {
+    let database = Database::new();
+
+    database.add_client(dummy_client("nick1"));
+    database.add_client(dummy_client("nick2"));
+
+    let expected = vec!["nick1".to_string(), "nick2".to_string()];
+    let mut real = database.get_all_clients();
+    real.sort();
+
+    assert_eq!(real, expected);
+}
+
+#[test]
 fn matches_works_for_inner_wilcard() {
     let stack = "hola_como_estas";
 
     assert!(matches(stack, "hola*estas"));
     assert!(matches(stack, "hola*como*estas"));
+    assert!(matches(stack, "*ola*como*estas"));
+    assert!(matches(stack, "hola*como*esta*"));
     assert!(!matches(stack, "Xola*como*estas"));
     assert!(!matches(stack, "hola*Xomo*estas"));
     assert!(!matches(stack, "hola*como*Xstas"));
     assert!(!matches(stack, "ola*como*estas"));
     assert!(!matches(stack, "hola*como*esta"));
-    assert!(matches(stack, "*ola*como*estas"));
-    assert!(matches(stack, "hola*como*esta*"));
 }
