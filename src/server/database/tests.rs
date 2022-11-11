c98e3b1e3e8f3f5ec59106a02b2ffa235a7bb902
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2022-11-11 15:58:54 -0300

    verify channel operator database test

--- src/server/database/tests.rs
+++ src/server/database/tests.rs
@@ -278,3 +278,16 @@ fn can_set_and_get_channel_topic() {
         Some("new topic".to_string())
     );
 }
+
+#[test]
+fn can_verify_channel_operator() {
+    let database = Database::start();
+
+    database.add_client(dummy_client("nickname1"));
+    database.add_client(dummy_client("nickname2"));
+    database.add_client_to_channel("nickname1", "#channel");
+    database.add_client_to_channel("nickname2", "#channel");
+
+    assert!(database.is_channel_operator("#channel", "nickname1"));
+    assert!(!database.is_channel_operator("#channel", "nickname2"));
+}
