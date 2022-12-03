e94b481f9b4b9103ca4363a18de037adc24d0c5d
Santiago Tadini <santi.tadini@gmail.com>
2022-12-03 10:35:54 -0300

    address arg

--- src/bin/view.rs
+++ src/bin/view.rs
@@ -1,7 +1,21 @@
-use internet_relay_chat::controller::Controller;
+use std::env;
+
+use internet_relay_chat::{controller::Controller, ADDRESS};
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let address = unpack_args(args);
+    
     let mut controller = Controller::new();
 
-    controller.start();
+    controller.start(address);
+}
+
+fn unpack_args(mut args: Vec<String>) -> String {
+    args.remove(0);
+
+    match args.pop() {
+        Some(address) => address,
+        None => ADDRESS.to_string(),
+    }
 }
