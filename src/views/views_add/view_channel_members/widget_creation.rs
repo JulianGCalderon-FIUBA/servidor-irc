db16a38cd8ba4d93e1ae93764366f5ee59e16a1d
Santiago Tadini <santi.tadini@gmail.com>
2022-12-04 16:10:08 -0300

    kick button

--- src/views/views_add/view_channel_members/widget_creation.rs
+++ src/views/views_add/view_channel_members/widget_creation.rs
@@ -1,11 +1,13 @@
 use gtk4 as gtk;
 
 use gtk::{
-    Align::Center, Box, Orientation::Horizontal,
+    Align::Center, Box, Orientation::Horizontal, Button, traits::{BoxExt},
 };
 use crate::views::widgets_creation::{
     create_label,
 };
+
+const KICK_LABEL: &str = "Kick";
 
 pub fn create_kick_label(member: String) -> Box {
     let label_box = Box::builder()
@@ -15,13 +17,27 @@ pub fn create_kick_label(member: String) -> Box {
         .margin_bottom(20)
         .build();
 
-    let label = create_label(&format!("\t •\tOP: {}", &member[1..]));
+    if member.starts_with("@") {
+        let label = create_label(&format!("\t •\tOP: {}", &member[1..]));
+        label_box.append(&label);
+    } else {
+        let label = create_label(&format!("\t •\t{}", member));
+        let kick_button = create_kick_button();
+        // connect_kick_button(kick_button, member);
 
-    let kick_button = create_kick_button(member);
+        label_box.append(&label);
+        label_box.append(&kick_button);
+    }
     
     label_box
 }
 
-fn create_kick_button(member: String) {
-
+fn create_kick_button() -> Button {
+    Button::builder().label(KICK_LABEL).build()
 }
+
+// fn connect_kick_button(kick_button: Button, member: String) {
+//     kick_button.connect_clicked(move |_| {
+
+//     });
+// }
