4ac2605fb97341505d534c3add49a195985cd3f5
Santiago Tadini <santi.tadini@gmail.com>
2023-02-27 13:33:51 -0300

    interface controller message_matcher.rs

--- src/controller/interface_controller/message_matcher.rs
+++ src/controller/interface_controller/message_matcher.rs
@@ -41,6 +41,7 @@ use super::{
 };
 
 impl InterfaceController {
+    /// Accept new dcc chat.
     pub fn accept_dcc_chat(&mut self, client: String, address: SocketAddr) {
         self.dcc_invitation_window.close();
         let dcc = self.dcc_receivers.remove(&client).unwrap();
@@ -67,12 +68,14 @@ impl InterfaceController {
         self.safe_conversation_window.insert(client, safe_view);
     }
 
+    /// Add a new client.
     pub fn add_new_client(&mut self, new_client: String) {
         self.add_client_window.close();
         self.main_view.add_client(&new_client);
         self.change_conversation(new_client.to_string());
     }
 
+    /// Change conversation.
     pub fn change_conversation(&mut self, current_conversation: String) {
         let last_conv: String = self.current_conv.clone();
         self.current_conv = current_conversation;
@@ -80,6 +83,7 @@ impl InterfaceController {
             .change_conversation(last_conv, self.current_conv.clone(), &self.dcc_chats);
     }
 
+    /// Close safe conversation.
     pub fn close_safe_view(&mut self, client: String) {
         self.dcc_chats.remove(&client);
         self.safe_conversation_view.remove(&client);
@@ -93,6 +97,7 @@ impl InterfaceController {
         self.main_view.update_safe_conversation_button(&client, &self.dcc_chats);
     }
 
+    /// Decline a new dcc chat.
     pub fn decline_dcc_chat(&mut self, client: String) {
         self.dcc_invitation_window.close();
         self.dcc_receivers
@@ -102,6 +107,7 @@ impl InterfaceController {
             .unwrap();
     }
 
+    /// Download new file.
     pub fn download_file(&mut self, sender: String, path: PathBuf) {
         let dcc_send_receiver = some_or_return!(self.dcc_send_receivers.remove(&sender));
 
@@ -122,16 +128,19 @@ impl InterfaceController {
         self.cancel_transfer_dialog("Download in progress", sender, controller);
     }
 
+    /// Show error view when adding a channel.
     pub fn error_when_adding_channel(&mut self, message: String) {
         self.add_channel_view.set_error_text(message);
     }
 
+    /// Ignores new file.
     pub fn ignore_file(&mut self, sender: String) {
         let dcc_send_receiver = some_or_return!(self.dcc_send_receivers.remove(&sender));
 
         dcc_send_receiver.decline_send_command().unwrap();
     }
 
+    /// Join a new channel.
     pub fn join_channel(&mut self, channel: String) {
         self.add_channel_window.close();
         let message: String = format!("{JOIN_COMMAND} {channel}");
@@ -139,11 +148,13 @@ impl InterfaceController {
         self.main_view.add_channel(channel);
     }
 
+    /// Kick a member from channel.
     pub fn kick_member(&mut self, channel: String, member: String) {
         let message: String = format!("{KICK_COMMAND} {channel} {member}");
         self.client.send(&message).expect(KICK_ERROR_TEXT);
     }
 
+    /// Opens the add channel view.
     pub fn open_add_channel_view(&mut self) {
         let channels: Vec<String> = self.process_list_end_message();
         let my_channels: Vec<String> = self.main_view.get_my_channels();
@@ -156,6 +167,7 @@ impl InterfaceController {
         self.add_channel_window.show();
     }
 
+    /// Opens the add client view.
     pub fn open_add_client_view(&mut self, channels_and_clients: HashMap<String, Vec<String>>) {
         let clients_not_mine: Vec<String> = self.clients_not_mine(channels_and_clients);
         if vec_is_not_empty(&clients_not_mine) {
@@ -166,7 +178,7 @@ impl InterfaceController {
         }
     }
 
-
+    /// Opens the file choose dialog view.
     pub fn open_file_chooser_dialog_view(&mut self) {
         let target = self.current_conv.clone();
         if target.is_empty() {
@@ -187,6 +199,7 @@ impl InterfaceController {
         connect_receiver_file_chooser(file_chooser_dialog, target, sender);
     }
 
+    /// Opens the invite client view.
     pub fn open_invite_client_view(&mut self, channels_and_clients: HashMap<String, Vec<String>>) {
         let my_channels: Vec<String> = self.main_view.get_my_channels();
         let current_conv_channels: Vec<String> = self.current_conv_channels(channels_and_clients);
@@ -199,6 +212,7 @@ impl InterfaceController {
         }
     }
 
+    /// Opens the main view.
     pub fn open_main_view(&mut self, message: Message) {
         let (nickname, realname, servername, username) = self.decode_registration(message);
 
@@ -213,10 +227,12 @@ impl InterfaceController {
         self.main_window.show();
     }
 
+    /// Opens the notifications view.
     pub fn open_notifications_view(&mut self) {
         notifications_window(&self.app, self.main_view.get_notifications()).show();
     }
 
+    /// Opens the reegister view.
     pub fn open_register_view(&mut self, address: String) {
         self.client = match Client::connect(address) {
             Ok(stream) => stream,
@@ -229,6 +245,7 @@ impl InterfaceController {
         self.register_window.show();
     }
 
+    /// Opens the user information view
     pub fn open_user_info_view(&mut self) {
         let nickname: String = self.nickname.clone();
         let realname: String = self.realname.clone();
@@ -237,16 +254,19 @@ impl InterfaceController {
         user_info_window(&self.app, nickname, realname, servername, username).show();
     }
 
+    /// Opens the warning view.
     pub fn open_warning_view(&mut self, message: String) {
         warning_window(&self.app, message).show();
     }
 
+    /// Receive a channel invitation.
     pub fn receive_invite(&mut self, message: Message) {
         let (channel, nickname) = self.decode_invite_message(message);
         let message: String = format!("{nickname} has invited you to join {channel}");
         self.main_view.add_notification(message);
     }
 
+    /// Receive a join message.
     pub fn receive_join(&mut self, message: Message) {
         let channel = self.decode_join_message(message);
 
@@ -255,6 +275,7 @@ impl InterfaceController {
         self.change_conversation(channel);
     }
 
+    /// Recevie a join notification.
     pub fn receive_join_notification(&mut self, message: Message) {
         let (channel, client) = self.decode_join_notification_message(message);
 
@@ -262,6 +283,7 @@ impl InterfaceController {
         self.main_view.add_notification(message);
     }
 
+    /// Receive a kick from channel.
     pub fn receive_kick(&mut self, message: Message) {
         let (channel, kicked) = self.decode_kick_message(message);
         if kicked == self.nickname {
@@ -272,11 +294,13 @@ impl InterfaceController {
         }
     }
 
+    /// Receive a list line message.
     pub fn receive_list_line(&mut self, message: Message) {
         let channel: String = self.decode_list_line_message(message);
         self.accumulated_channels_from_list.push(channel);
     }
 
+    /// Receive a names end message.
     pub fn receive_names_end(&mut self) {
         let channels_and_clients: HashMap<String, Vec<String>> = self.process_names_end_message();
 
@@ -306,12 +330,14 @@ impl InterfaceController {
         }
     }
 
+    /// Receive a names line message.
     pub fn receive_names_line(&mut self, message: Message) {
         let (channels, clients) = self.decode_names_line_message(message);
         self.accumulated_channels_from_names.push(channels);
         self.accumulated_clients_from_names.push(clients);
     }
 
+    /// Receive a private message.
     pub fn receive_priv_message(&mut self, message: Message) {
         match parse_ctcp(&message) {
             Some(message_text) => {
@@ -323,6 +349,7 @@ impl InterfaceController {
         }
     }
 
+    /// Receive a transfer result
     pub fn receive_result(&mut self, sender: String, name: String, result: io::Result<()>) {
         let position = self
             .downloads
@@ -339,6 +366,7 @@ impl InterfaceController {
         self.transfer_result(result, sender);
     }
 
+    /// Receive a safe message
     pub fn receive_safe_message(&mut self, client: String, message: String) {
         let safe_conversation = self.safe_conversation_view.remove(&client);
         if let Some(mut safe_view) = safe_conversation {
@@ -347,6 +375,7 @@ impl InterfaceController {
         }
     }
 
+    /// Register in the server.
     pub fn register(&mut self, pass: String, nickname: String, username: String, realname: String) {
         let pass_command: String = format!("{PASS_COMMAND} {pass}");
         let nick_command: String = format!("{NICK_COMMAND} {nickname}");
@@ -357,16 +386,19 @@ impl InterfaceController {
         self.client.send(&user_command).expect(USER_ERROR_TEXT);
     }
 
+    /// Receive a regular message.
     pub fn regular_message(&mut self, message: String) {
         println!("{message}");
     }
 
+    /// Remove conversation
     pub fn remove_conversation(&mut self) {
         self.main_view
             .remove_conversation(self.current_conv.clone());
         self.main_view.welcome_view();
     }
 
+    /// Sends a dcc send request.
     pub fn send_dcc_send(&mut self, target: String, path: PathBuf) {
         let server_stream = self.get_stream();
         let dcc_send_sender = DccSendSender::send(server_stream, target.clone(), path).unwrap();
@@ -374,30 +406,36 @@ impl InterfaceController {
         self.dcc_send_senders.insert(target, dcc_send_sender);
     }
 
+    /// Send an invite message.
     pub fn send_invite_message(&mut self, channel: String) {
         self.invite_window.close();
         let invite: String = format!("{INVITE_COMMAND} {} {channel}", self.current_conv);
         self.client.send(&invite).expect(INVITE_ERROR_TEXT);
     }
 
+    /// Send a join message.
     pub fn send_join_message(&mut self, channel: String) {
         let message: String = format!("{JOIN_COMMAND} {channel}");
         self.client.send(&message).expect(JOIN_ERROR_TEXT);
     }
 
+    /// Send a kick message.
     pub fn send_kick_message(&mut self, channel: String, member: String) {
         let message: String = format!("{KICK_COMMAND} {channel} {member}");
         self.client.send(&message).expect(KICK_ERROR_TEXT);
     }
 
+    /// Send a list message.
     pub fn send_list_message(&mut self) {
         self.client.send(LIST_COMMAND).expect(LIST_ERROR_TEXT);
     }
 
+    /// Send names message to add client.
     pub fn send_names_message_to_add_client(&mut self) {
         self.send_names_message(AddClient, None);
     }
 
+    /// Send names message to invite client.
     pub fn send_names_message_to_invite_client(&mut self) {
         let my_channels: Vec<String> = self.main_view.get_my_channels();
         if vec_is_not_empty(&my_channels) {
@@ -407,29 +445,35 @@ impl InterfaceController {
         }
     }
 
+    /// Send names message to know members.
     pub fn send_names_message_to_know_members(&mut self) {
         self.send_names_message(KnowMembers, Some(self.current_conv.clone()));
     }
 
+    /// Send a part message.
     pub fn send_part_message(&mut self) {
         let part_message: String = format!("{PART_COMMAND} {}", self.current_conv);
         self.client.send(&part_message).expect(PART_ERROR_TEXT);
     }
 
+    /// Send a private message.
     pub fn send_priv_message(&mut self, message: String) {
         let priv_message = format!("{PRIVMSG_COMMAND} {} :{message}", self.current_conv);
         self.client.send(&priv_message).expect(PRIVMSG_ERROR_TEXT);
         self.main_view.send_message(message);
     }
 
+    /// Send a quit message.
     pub fn send_quit_message(&mut self) {
         self.client.send(QUIT_COMMAND).expect(QUIT_ERROR_TEXT);
     }
 
+    /// Send a transfer result
     pub fn send_result(&mut self, sender: String, result: io::Result<()>) {
         self.transfer_result(result, sender);
     }
 
+    /// Send a safe conversation request.
     pub fn send_safe_conversation_request(&mut self) {
         let stream = self.get_stream();
         let chat_sender = DccChatSender::send(stream, self.current_conv.clone()).unwrap();
@@ -437,6 +481,7 @@ impl InterfaceController {
             .insert(self.current_conv.clone(), chat_sender);
     }
 
+    /// Send a safe message.
     pub fn send_safe_message(&mut self, receiver_client: String, message: String) {
         let dcc = self.dcc_chats.remove(&receiver_client);
         if let Some(mut dcc_chat) = dcc {
@@ -447,8 +492,6 @@ impl InterfaceController {
                 safe_view.send_message(message, receiver_client.clone());
                 self.safe_conversation_view.insert(receiver_client, safe_view);
             }
-            // self.safe_conversation_view
-            //     .send_message(message, receiver_client);
         }
     }
 }
