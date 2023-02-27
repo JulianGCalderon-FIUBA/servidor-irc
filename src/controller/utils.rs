06bfe67b031387200a36b62661e5967ac091ba00
Santiago Tadini <santi.tadini@gmail.com>
2023-02-27 12:29:10 -0300

    controller utils.rs

--- src/controller/utils.rs
+++ src/controller/utils.rs
@@ -53,11 +53,17 @@ pub fn client_channels(
     client_channels_set
 }
 
+/// Returns first word of a button.
+/// 
+/// Receives a reference to a Button and returns a String. 
 pub fn first_word_of_button(button: &Button) -> String {
     let text = button.label().unwrap().to_string();
     text.split_whitespace().collect::<Vec<&str>>()[0].to_string()
 }
 
+/// Returns a sender and a receiver.
+/// 
+/// Receives nothing, returns a String Sender and a String Receiver.
 pub fn get_sender_and_receiver() -> (Sender<String>, Receiver<String>) {
     MainContext::channel(PRIORITY_DEFAULT)
 }
@@ -69,22 +75,34 @@ pub fn is_channel(parameter: &str) -> bool {
     parameter.starts_with(CHANNEL_FIRST_CHARACTER)
 }
 
+/// Returns a bool indicating if the string is empty.
+/// 
+/// Receives a reference to a string, returns a bool.
 pub fn is_not_empty(text: &str) -> bool {
     !text.is_empty()
 }
 
+/// Pushes an element to a vector only if the element is not in the vector.
+/// 
+/// Receives two references to a string vector and a String element, returns nothing.
 pub fn push_if_absent(original_vector: &[String], new_vector: &mut Vec<String>, element: String) {
     if !original_vector.contains(&element) {
         new_vector.push(element);
     }
 }
 
+/// Removes element from a vector only if it is present.
+/// 
+/// Receives a reference to a string vector and a reference to a string, returns nothing.
 pub fn remove_element(vector: &mut Vec<String>, element: &String) {
     if vector.contains(element) {
         vector.remove(vector.iter().position(|x| x == element).unwrap());
     }
 }
 
+/// Removes the operator indicator from a string.
+/// 
+/// Receives a string reference, returns a string.
 pub fn remove_operator_indicator(element: &str) -> String {
     if let Some(stripped) = element.strip_prefix(OPERATOR_CHARACTER) {
         stripped.to_string()
@@ -93,6 +111,9 @@ pub fn remove_operator_indicator(element: &str) -> String {
     }
 }
 
+/// Returns a bool indicating if the vector is not empty.
+/// 
+/// Receives a reference to a vector, returns a bool.
 pub fn vec_is_not_empty(vector: &Vec<String>) -> bool {
     !vector.is_empty()
 }
